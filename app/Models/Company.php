5600ceca91cda70374536df34424da3b3f31c2bd
Vanja Opacic <vanjaopacic@primer.com>
2024-09-08 03:11:22 +0200

    Added models and relationships

---
+++ app/Models/Company.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'industry'
+    ];
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function leads()
+    {
+        return $this->hasMany(Lead::class);
+    }
 }
